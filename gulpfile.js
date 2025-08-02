39f831b3ca1514fa2f76be493531eb88df4ae852
Darren Embry <dse@webonastick.com>
2025-08-02 18:36:44 -0400

    additions/changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,52 @@ import autoprefixer  from 'autoprefixer';
 import browsersync   from 'browser-sync';
 import babel         from '@rollup/plugin-babel';
 import * as dartSass from 'sass';
+import data          from 'gulp-data';
+import fs            from 'node:fs';
 import gulp          from 'gulp';
 import gulpSass      from 'gulp-sass';
 import postcss       from 'gulp-postcss';
 import resolve       from '@rollup/plugin-node-resolve';
 import * as rollup   from 'rollup';
+import nunjucks      from 'gulp-nunjucks-render';
+import { globSync }  from 'glob';
+
+const dist = isDev() ? '_dev/dist' : 'dist';
+fs.mkdirSync(dist, { recursive: true });
 
 const sass = gulpSass(dartSass);
 let server;
+
+function createSiteData() {
+    const obj = globSync('src/data/**/*.json').reduce(
+        (accum, next) => Object.assign(accum, next),
+        {}
+    );
+    obj.cacheBuster = Date.now();
+    console.log(JSON.stringify(obj, null, 4));
+    return obj;
+}
 
 function sassTask() {
     console.log(`sassTask: executing`);
     return gulp.src('src/styles/app.scss')
                .pipe(sass())
                .pipe(postcss([autoprefixer]))
-               .pipe(gulp.dest('dist/styles'));
+               .pipe(gulp.dest(`${dist}/styles`));
 }
 
 function htmlTask() {
     console.log(`htmlTask: executing`);
     return gulp.src('src/pages/**/*.html')
-               .pipe(gulp.dest('dist'));
+               .pipe(data(createSiteData))
+               .pipe(nunjucks({ path: 'src/pages' }))
+               .pipe(gulp.dest(`${dist}`));
+}
+
+function nunjucksTask() {
+    return gulp.src('src/pages/**/*.njk')
+               .pipe(nunjucks())
+               .pipe(gulp.dest(`${dist}`));
 }
 
 function rollupTask() {
@@ -35,7 +60,7 @@ function rollupTask() {
         .then(bundle => {
             console.log(`rollupTask: generating bundle`);
             return bundle.write({
-                file: './dist/scripts/app.js',
+                file: `./${dist}/scripts/app.js`,
                 format: 'umd',
                 name: 'library',
             });
@@ -50,7 +75,7 @@ function serverTask() {
     console.log(`serverTask: starting server`);
     server = browsersync.create();
     server.init({
-        server: './dist',
+        server: ['public', `${dist}`]
     });
 }
 
@@ -70,15 +95,26 @@ function watchTask() {
     gulp.watch('src/scripts/**/*.js', gulp.series(rollupTask, reloadTask));
 }
 
-const buildTask = gulp.parallel(sassTask, htmlTask, rollupTask);
+function staticTask() {
+    return gulp.src('public/**/*').pipe(gulp.dest(`${dist}`));
+}
+
+const buildTask = gulp.parallel(sassTask, htmlTask, rollupTask, staticTask);
 
 const devTask = gulp.series(
     gulp.parallel(sassTask, htmlTask, rollupTask),
     gulp.parallel(serverTask, watchTask),
 );
 
-export { rollupTask as rollup };
-export { sassTask   as sass   };
-export { htmlTask   as html   };
-export { buildTask  as build  };
-export { devTask    as dev    };
+export { rollupTask   as rollup   };
+export { sassTask     as sass     };
+export { htmlTask     as html     };
+export { buildTask    as build    };
+export { devTask      as dev      };
+export { staticTask   as static   };
+export { nunjucksTask as nunjucks };
+
+function isDev() {
+    const env = process.env.NODE_ENV;
+    return env === "dev" || (env != null && env !== 'production');
+}
