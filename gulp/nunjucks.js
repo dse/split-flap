8b757bfceb6b140125cbc6a78b3433734e1ee289
Darren Embry <dse@webonastick.com>
2025-08-03 21:33:55 -0400

    use config.cjs

--- gulp/nunjucks.js
+++ gulp/nunjucks.js
@@ -4,9 +4,12 @@ import nunjucks from 'gulp-nunjucks-render';
 import createSiteData from './site-data.js';
 import { DIST, EXCLUDE_PARTIALS } from './constants.js';
 
+import config from './config.cjs';
+const { nunjucksConfig } = config;
+
 export default function nunjucksTask() {
     return gulp.src(['src/pages/**/*.njk', ...EXCLUDE_PARTIALS], { base: 'src/pages' })
                .pipe(data(createSiteData))
-               .pipe(nunjucks({ path: 'src/pages' }))
+               .pipe(nunjucks(nunjucksConfig))
                .pipe(gulp.dest(`${DIST}`));
 }
