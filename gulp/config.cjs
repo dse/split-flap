a311d53d68b35d6dec615477518b4e5945163523
Darren Embry <dse@webonastick.com>
2025-08-03 21:33:44 -0400

    reorder making sure .buildconfig.js is never there

--- gulp/config.cjs
+++ gulp/config.cjs
@@ -6,10 +6,12 @@ const path = require('node:path');
 
 let config;
 
-if (fs.existsSync('.buildconfig.json')) {
-    config = JSON.parse(fs.readFileSync('.buildconfig.json').toString());
+if (fs.existsSync('.buildconfig.js')) {
+    throw new Error(`.buildconfig.js not supported!  You must use .buildconfig.cjs.`);
 } else if (fs.existsSync('.buildconfig.cjs')) {
     config = require(path.resolve('.buildconfig.cjs'));
+} else if (fs.existsSync('.buildconfig.json')) {
+    config = JSON.parse(fs.readFileSync('.buildconfig.json').toString());
 } else {
     config = {};
 }
