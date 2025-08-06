84ca3fe516860a4440d00a58b2658651704af291
Darren Embry <dse@webonastick.com>
2025-08-06 01:13:53 -0400

    need separate build for pwa-sw.js.

--- gulp/rollup.js
+++ gulp/rollup.js
@@ -1,9 +1,10 @@
+import gulp from 'gulp';
 import babel from '@rollup/plugin-babel';
 import * as rollup from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import { DIST } from './constants.js';
 
-export default function rollupTask() {
+export function rollupBuild1() {
     return rollup
         .rollup({ input: 'src/scripts/split-flap/clock-page.js',
                   plugins: [resolve(), babel({ babelHelpers: 'bundled' })] })
@@ -16,3 +17,21 @@ export default function rollupTask() {
             });
         });
 }
+
+export function rollupBuild2() {
+    return rollup
+        .rollup({ input: 'src/scripts/split-flap/pwa-sw.js',
+                  plugins: [resolve(), babel({ babelHelpers: 'bundled' })] })
+        .then(bundle => {
+            const filename = `./${DIST}/split-flap/scripts/pwa-sw.js`;
+            return bundle.write({
+                file: filename,
+                format: 'umd',
+                name: 'library',
+            });
+        });
+}
+
+const rollupTask = gulp.parallel(rollupBuild1, rollupBuild2);
+
+export default rollupTask;
